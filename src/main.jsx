22785c4f14e508047af7ecee986f62945958e47b
Abdullah <bh532997@gmail.com>
2023-07-13 19:31:37 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Navbar from './components/Navbar'
+// import Home from './components/Home'
+// import Num from './components/Num'
+// import Business from './components/Business'
+// import Easily from './components/Easily'
+// import Find from './components/Find'
+// import Cards from './components/Cards'
+// import Markets from './components/Markets'
+// import Try from './components/Try'
+// import Footer from './components/Footer'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <Navbar />
+    <Home />
+    <Num />
+    <Business />
+    <Easily />
+    <Find />
+    <Cards />
+    <Markets />
+    <Try />
+    <Footer /> */}
+  </React.StrictMode>,
+)
