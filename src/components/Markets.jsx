bbd11c44aef69fa998ff86566d34f900467123a2
Abdullah <bh532997@gmail.com>
2024-04-14 20:52:42 +0300

    marquee animation

--- src/components/Markets.jsx
+++ src/components/Markets.jsx
@@ -4,23 +4,42 @@ import binance from "../assets/binance.png"
 import coinbase from "../assets/coinbase.png"
 import dropbox from "../assets/dropbox.png"
 
+
 export default function Markets() {
     return (
-        <div className="market space" id="three">
-            <div className="container grid grid-cols-4 max-[767px]:grid-cols-2 gap-4">
-                <div className="pho w-full flex justify-center items-center">
-                    <LazyLoadImage src={airbnb} alt="" className="w-2/3" />
+        <div className="market  mb-16" id="three">
+
+            <div className="relative flex overflow-x-hidden">
+                <div className="py-12 animate-marquee whitespace-nowrap flex justify-between items-center">
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={airbnb} alt="" className="w-2/3" />
+                    </span>
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={binance} alt="" className="w-2/3" />
+                    </span>
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={coinbase} alt="" className="w-2/3" />
+                    </span>
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={dropbox} alt="" className="w-2/3" />
+                    </span>
                 </div>
-                <div className="pho w-full flex justify-center items-center">
-                    <LazyLoadImage src={binance} alt="" className="w-2/3" />
-                </div>
-                <div className="pho w-full flex justify-center items-center">
-                    <LazyLoadImage src={coinbase} alt="" className="w-2/3" />
-                </div>
-                <div className="pho w-full flex justify-center items-center">
-                    <LazyLoadImage src={dropbox} alt="" className="w-2/3" />
+                <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap flex justify-between items-center">
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={airbnb} alt="" className="w-2/3" />
+                    </span>
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={binance} alt="" className="w-2/3" />
+                    </span>
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={coinbase} alt="" className="w-2/3" />
+                    </span>
+                    <span className="text-4xl mx-4">
+                        <LazyLoadImage src={dropbox} alt="" className="w-2/3" />
+                    </span>
                 </div>
             </div>
+
         </div>
     )
 }
