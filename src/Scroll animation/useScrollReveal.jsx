d58d8cc04e18a36b4e585b7e6b47c14e664dcc92
Abdullah <bh532997@gmail.com>
2024-04-14 15:00:33 +0300

    play store button

--- src/Scroll animation/useScrollReveal.jsx
+++ src/Scroll animation/useScrollReveal.jsx
@@ -74,6 +74,5 @@ export const useScrollReveal = () => {
             easing: "ease"
         });
 
-
     }, []);
 };
--- src/components/Easily.jsx
+++ src/components/Easily.jsx
@@ -1,6 +1,7 @@
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import bill from "../assets/bill.png"
 import apple from "../assets/apple.svg"
+import play from "../assets/play.svg"
 
 export default function Easily() {
     return (
@@ -15,8 +16,12 @@ export default function Easily() {
                     <h1 className="font-bold leading-normal text-4xl">Easily control your billing & invoicing.</h1>
                     <p className="txt my-5">Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
                     <div className="buttons flex justify-start gap-4 items-center max-[799px]:justify-center">
-                        <LazyLoadImage src={apple} alt="" />
-                        <LazyLoadImage src={apple} alt="" />
+                        <button>
+                            <LazyLoadImage src={apple} alt="" />
+                        </button>
+                        <button>
+                            <LazyLoadImage src={play} alt="" />
+                        </button>
                     </div>
                 </div>
             </div>
